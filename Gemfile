9ce78a3e49fb324d81947e3f1b52b554302970af
taimushimizu <213.f.r48132@gmail.com>
2020-03-19 19:11:01 +0900

    facebookのOauth導入

--- Gemfile
+++ Gemfile
@@ -29,10 +29,12 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
-
+gem 'dotenv-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+   def self.find_for_oauth(auth)
+     user = User.where(uid: auth.uid, provider: auth.provider).first
+     unless user
+       user = User.create(
+         uid:      auth.uid,
+         provider: auth.provider,
+         email:    auth.info.email,
+         name:  auth.info.name,
+         password: Devise.friendly_token[0, 20],
+         image:  auth.info.image
+       )
+     end
+     user
+   end
+end
