c8f09ee05883f815e7ac4a7a9a5a1723ca896715
taimushimizu <213.f.r48132@gmail.com>
2020-03-21 19:13:32 +0900

    ユーザー編集後ユーザーページに飛ぶよう実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  
+  def after_update_path_for(resource)
+    user_path(id: current_user.id)
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
