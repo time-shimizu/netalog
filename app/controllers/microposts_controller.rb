7c980b5faa8db2bfbc9698e5654f4aa204268f3d
taimushimizu <213.f.r48132@gmail.com>
2020-03-21 20:35:00 +0900

    投稿にyoutube動画が埋め込まれるよう実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,6 +10,17 @@ class MicropostsController < ApplicationController
 
   def new
     @micropost = current_user.micropsts.build(micropost_params)
+    youtube_url = params[:micropost][:url]
+    youtube_url = youtube_url.last(11)
+    @post.url = youtube_url
+
+    if @post.save
+      flash[:success] = "投稿が完了しました"
+      redirect_to @post
+    else
+      flash[:danger] = "投稿に失敗しました"
+      render "new"
+    end
   end
 
   def edit
