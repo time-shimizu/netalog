59158bf0f27268a22727249ac236f6983ec99daf
taimushimizu <213.f.r48132@gmail.com>
2020-03-23 18:38:08 +0900

    カテゴリーモデル、サブカテゴリーモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_101950) do
+ActiveRecord::Schema.define(version: 2020_03_23_093439) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.string "title"
@@ -21,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_03_21_101950) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/subcategories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :subcategory do
+    name "MyString"
+    category nil
+  end
+end
