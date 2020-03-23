2c75d82678d94d9d945228c91409ef0eb83f1646
taimushimizu <213.f.r48132@gmail.com>
2020-03-24 06:49:53 +0900

    フォロー、アンフォローボタンの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   validates :profile, length: { maximum: 200 }
   mount_uploader :image, ImageUploader
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
    def self.find_for_oauth(auth)
      user = User.where(uid: auth.uid, provider: auth.provider).first
@@ -21,6 +25,18 @@ class User < ApplicationRecord
      end
      user
    end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
    private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :following, :followers] do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts
   resources :subcategories, only: [:show]
   resources :scores, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "staticpages#home"
 end
