c33e07c9af5c0653752c838142a348dd3dbd7706
taimushimizu <213.f.r48132@gmail.com>
2020-03-26 00:02:59 +0900

    Topページで人気ユーザーが表示されるよう追記

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,4 +1,7 @@
 class StaticpagesController < ApplicationController
   def home
+    @users = User.find(Relationship.group(:followed_id).order('count(followed_id) desc').limit(4).pluck(:followed_id))
+    @new_post = Micropost.all.take(3)
+    @popular_post = 
   end
 end
