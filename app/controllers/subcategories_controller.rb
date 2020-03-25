01a5d8facbc27779e5e18697235df287548485de
taimushimizu <213.f.r48132@gmail.com>
2020-03-26 00:48:39 +0900

    サブカテゴリーごとに投稿を表示する時、作成時間で降順に変更

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -1,7 +1,7 @@
 class SubcategoriesController < ApplicationController
   def show
     @subcategory = Subcategory.find(params[:id])
-    @microposts = @subcategory.microposts.page(params[:page]).per(5)
+    @microposts = @subcategory.microposts.order('created_at desc').page(params[:page]).per(5)
     @categories = Category.all
     render "microposts/index"
   end
