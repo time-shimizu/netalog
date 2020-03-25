16e2905167046c11b0356cea824b5e364015df40
taimushimizu <213.f.r48132@gmail.com>
2020-03-26 00:51:05 +0900

    ユーザーページも投稿順を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.page(params[:page]).per(5)
+    @microposts = @user.microposts.order('created_at desc').page(params[:page]).per(5)
   end
 
   def following
