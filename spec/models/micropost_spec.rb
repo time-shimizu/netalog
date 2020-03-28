23aaa70f8713ef72a7899a6eb67bb78b427c14ba
taimushimizu <213.f.r48132@gmail.com>
2020-03-28 19:29:41 +0900

    micropostのモデルspecを記載

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:subcategory) {create(:subcategory)}
+  let!(:micropost) {create(:micropost, subcategory: subcategory)}
+
+  context "title contentがある時" do
+    it "micropostは有効である" do
+      expect(micropost).to be_valid
+    end
+  end
+
+  context "titleがnilの時" do
+    it "バリデーションにかかる" do
+      micropost.title = nil
+      micropost.valid?
+      expect(micropost.errors[:title]).to include "を入力してください"
+    end
+  end
+
+  context "contentがnilの時" do
+    it "バリデーションにかかる" do
+      micropost.content = nil
+      micropost.valid?
+      expect(micropost.errors[:content]).to include "を入力してください"
+    end
+  end
+
+  context "subcategoryがnilの時" do
+    it "バリデーションにかかる" do
+      micropost.subcategory_id = nil
+      expect(micropost).not_to be_valid
+    end
+  end
 end
