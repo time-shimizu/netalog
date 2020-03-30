f956f352eda0e88a0c03f368bef8d9fd5668101d
taimushimizu <213.f.r48132@gmail.com>
2020-03-30 16:43:48 +0900

    検索機能の統合テストの追加

--- spec/features/searches_spec.rb
+++ spec/features/searches_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Searches", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let!(:user) {create(:user)}
+  let!(:category) {create(:category)}
+  let!(:subcategory) { create(:subcategory, name: "しゃべくり漫才")}
+  let!(:micropost1) {create(:micropost, title: "first1", content: "first1", user: user, subcategory: subcategory)}
+  let!(:micropost2) {create(:micropost, title: "second2", content: "second2", user: user, subcategory: subcategory)}
+  let!(:micropost3) {create(:micropost, title: "first3", content: "second3", user: user, subcategory: subcategory)}
+  let!(:micropost4) {create(:micropost, title: "second4", content: "first4", user: user, subcategory: subcategory)}
+
+  scenario "検索機能が正しく働く" do
+    visit microposts_path
+    fill_in "search", with: "first"
+    click_button "検索"
+    expect(page).to have_content micropost1.title
+    expect(page).not_to have_content micropost2.title
+    expect(page).to have_content micropost3.title
+    expect(page).to have_content micropost4.title
+  end
 end
