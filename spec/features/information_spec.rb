56d7720590dea6124746b18a85c358a0b63652ef
taimushimizu <213.f.r48132@gmail.com>
2020-03-30 17:04:40 +0900

    採点と通知の統合テスト追加

--- spec/features/information_spec.rb
+++ spec/features/information_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.feature "Information", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let!(:user) {create(:user)}
+  let!(:other_user) {create(:user)}
+  let!(:category) {create(:category)}
+  let!(:subcategory) {create(:subcategory, category: category)}
+  let!(:micropost) {create(:micropost, user: user, subcategory: subcategory)}
+
+  scenario "ユーザーがフォローされたら通知が届く" do
+    visit root_path
+    click_link "ログイン"
+    fill_in "メールアドレス", with: other_user.email
+    fill_in "パスワード",    with: other_user.password
+    click_button "ログイン"
+
+    expect{
+      visit user_path(user)
+      click_button "フォローする"
+    }.to change{user.infos.count}.by(1)
+  end
+
+  scenario "ユーザーの投稿に返信があったら通知が届く" do
+    visit root_path
+    click_link "ログイン"
+    fill_in "メールアドレス", with: other_user.email
+    fill_in "パスワード",    with: other_user.password
+    click_button "ログイン"
+
+    expect{
+      visit micropost_path(micropost.id)
+      fill_in "reply_content", with: "sample reply"
+      click_button "返信する"
+
+      expect(page).to have_content "sample reply"
+    }.to change{user.infos.count}.by(1)
+  end
 end
--- spec/features/scores_spec.rb
+++ spec/features/scores_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "Scores", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let!(:user) {create(:user)}
+  let!(:category) {create(:category)}
+  let!(:subcategory) {create(:subcategory, category: category)}
+  let!(:micropost) {create(:micropost, user: user, subcategory: subcategory)}
+
+  scenario "ユーザーがログイン後、採点できる" do
+    visit root_path
+    click_link "ログイン"
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect{
+      visit micropost_path(micropost.id)
+      select "1", from: "point"
+      click_button "採点する"
+
+      expect(page).to have_content "1.0"
+    }.to change{Score.count}.by(1)
+  end
 end
